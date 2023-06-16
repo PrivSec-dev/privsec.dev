8567512998cacb82163d9a2966fdbadef64e5ce8
Tommy <contact@tommytran.io>
2023-06-16 05:19:15 -0700

    Use the latest Hugo version
    Signed-off-by: Tommy <contact@tommytran.io>

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export GOPROXY=direct
+export GOSUMD=off
+export CGO_ENABLED=1
+
+go install -tags extended github.com/gohugoio/hugo@latest
+
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   hugo --minify
 else
