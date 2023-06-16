a881a83a31ec8cf2b7b2c987b18e3cc96a9e2209
samsepi0l <contact@simpleprivacy.fr>
2023-06-16 15:11:22 +0200

    Shellchecked
    Signed-off-by: samsepi0l <contact@simpleprivacy.fr>

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -2,11 +2,11 @@
 
 curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
 
-tar xvf *.tar.gz
+tar xvf ./*.tar.gz
 chmod u+x ./hugo
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   ./hugo --minify
 else
-  ./hugo -b $CF_PAGES_URL --minify
+  ./hugo -b "$CF_PAGES_URL" --minify
 fi
