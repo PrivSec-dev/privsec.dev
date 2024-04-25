314273c637511677f0ad8465b6f584232d435ed4
Tommy <contact@tommytran.io>
2024-04-24 22:30:58 -0700

    Pin Hugo v0.119.0
    Signed-off-by: Tommy <contact@tommytran.io>

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
+#curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
+
+wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_Linux-64bit.tar.gz
 
 tar xvf ./*.tar.gz
 chmod u+x ./hugo
